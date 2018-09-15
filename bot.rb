68d70cd879b21fb1865e1220bc375f6837339528
Jan Klimo <jan.klimo@gmail.com>
2018-09-15 23:33:16 +0700

    Logging

--- bot.rb
+++ bot.rb
@@ -41,7 +41,7 @@ class API < Sinatra::Base
   end
 
   post '/command' do
-    STDOUT.puts params
+    STDERR.puts params
     font = Figlet::Font.new(font_path('banner'))
     figlet = Figlet::Typesetter.new(font)
     status 200
